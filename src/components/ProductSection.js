1621d0ec85136890182f28c605e5d1e41705730f
sud02 <87568742+sud02@users.noreply.github.com>
2024-09-06 20:26:25 +0530

    price fixs

--- src/components/ProductSection.js
+++ src/components/ProductSection.js
@@ -5,10 +5,10 @@ import './ProductSection.css';
 const ProductSection = () => {
   const navigate = useNavigate();
   const products = [
-    { img: '/Static/ProductImgs/FIRE.jpg', alt: 'Cool School Shirt', name: 'FIRE T-SHIRT', price: 'RS. 5,995', id: 1 },
-    { img: '/Static/ProductImgs/FLORAL.jpg', alt: 'Crystal T Blue T-Shirt', name: 'FLORAL T-SHIRT', price: 'RS. 5,995', id: 2 },
-    { img: '/Static/ProductImgs/LIVE.jpg', alt: 'Crystal Black T-Shirt', name: 'LITM T-SHIRT', price: 'RS. 5,995', id: 3 },
-    { img: '/Static/ProductImgs/PIGEON.jpg', alt: 'Black White Polo T-Shirt', name: 'PIGEON T-SHIRT', price: 'RS. 4,495', id: 4 },
+    { img: '/Static/ProductImgs/FIRE.jpg', alt: 'Cool School Shirt', name: 'FIRE T-SHIRT', price: 'RS. 1,999', id: 1 },
+    { img: '/Static/ProductImgs/FLORAL.jpg', alt: 'Crystal T Blue T-Shirt', name: 'FLORAL T-SHIRT', price: 'RS. 1,999', id: 2 },
+    { img: '/Static/ProductImgs/LIVE.jpg', alt: 'Crystal Black T-Shirt', name: 'LITM T-SHIRT', price: 'RS. 1,999', id: 3 },
+    { img: '/Static/ProductImgs/PIGEON.jpg', alt: 'Black White Polo T-Shirt', name: 'PIGEON T-SHIRT', price: 'RS. 1,999', id: 4 },
   ];
 
   const handleClick = (productId) => {
