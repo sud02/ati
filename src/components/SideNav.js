5055785c775dd50de54018c5a0c19a2c0d90a932
sud02 <87568742+sud02@users.noreply.github.com>
2024-09-14 16:11:44 +0530

    login fuctionality

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom';
 import './Sidenav.css';
 
 function SideNav({ isOpen, closeSideNav }) {
+    const firstName = localStorage.getItem('firstName');
+
     return (
         <div className={`side-nav ${isOpen ? 'open' : ''}`}>
             <div className="side-nav-content">
-            <button 
-    className="close-btn" 
-    onClick={closeSideNav} 
-    aria-label="Close Side Navigation"
->
-    &times;
-</button>
-
+                <button 
+                    className="close-btn" 
+                    onClick={closeSideNav} 
+                    aria-label="Close Side Navigation"
+                >
+                    &times;
+                </button>
+                
+                {firstName && <div className="user-greeting">Hello, {firstName}!</div>}
+                
                 <ul>
                     <li>
                         <Link to="/shop-all" onClick={closeSideNav}>Shop All</Link>
@@ -25,7 +29,6 @@ function SideNav({ isOpen, closeSideNav }) {
                         <Link to="/track-order" onClick={closeSideNav}>Track Order</Link>
                     </li>
                 </ul>
-
             </div>
         </div>
     );
